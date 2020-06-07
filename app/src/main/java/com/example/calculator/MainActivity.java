c63872f3c3a1af7439b5403826bf2e8c0fd022e8
TAYBI <taybib4@gmail.com>
2020-06-07 19:41:06 +0100

    inputs ui

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.calculator;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText result;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (EditText)findViewById(R.id.result);
+
+        result.setShowSoftInputOnFocus(false);
+    }
+
+    void print(String str) {
+        result.setText(result.getText() + str);
     }
 }
